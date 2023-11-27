670c307c07bdd67ef1cd5f00899275d96f0f0ce2
peterintouch <82558492+peterintouch@users.noreply.github.com>
2023-11-28 12:13:59 +1300

    seperate maintenance and slower schedule

--- maintenance-updates.sh
+++ maintenance-updates.sh
@@ -6,7 +6,7 @@
 # other scripts could technically be running the same processes
 # it's better to just check for those directly
 #
-IS_RUNNING=$(pgrep -fa "console mautic:(campaigns|import|webhook|reports|process|scheduler)")
+IS_RUNNING=$(pgrep -fa "console mautic:(maintenance|cleanup|unusedip)")
 LOGFILE="$(dirname "$0")/logs/mautic_quick_cron.log"
 
 # Common vars and functions
@@ -20,22 +20,13 @@ if [ -z "$IS_RUNNING" ]; then
    if [[ $(ip a | grep $MAUTIC_VIP &> /dev/null; echo $?) == 0 ]]; then
       verbose "Did NOT find a currently running script, beginning execution"
 
-      verbose "Running mautic:campaigns:update"
-      php $MAUTIC_BASE/bin/console mautic:campaigns:update $MAUTIC_OPTS $CAMPAIGNS_BATCH_LIMIT >> $LOGFILE
-
-      verbose "Running mautic:campaigns:trigger"
-      php $MAUTIC_BASE/bin/console mautic:campaigns:trigger $MAUTIC_OPTS $CAMPAIGNS_BATCH_LIMIT >> $LOGFILE
-
-      verbose "Running mautic:import"
-      php $MAUTIC_BASE/bin/console mautic:import $MAUTIC_OPTS >> $LOGFILE
-
-      verbose "Running mautic:webhooks:process"
-      php $MAUTIC_BASE/bin/console mautic:webhooks:process $MAUTIC_OPTS >> $LOGFILE
-
       verbose "Running mautic:reports:scheduler"
-      php $MAUTIC_BASE/bin/console mautic:reports:scheduler $MAUTIC_OPTS >> $LOGFILE
+      php $MAUTIC_BASE/bin/console mautic:unusedip:delete $MAUTIC_OPTS >> $LOGFILE
 
+      verbose "Running mautic:maintenance:cleanup"
+      php $MAUTIC_BASE/bin/console mautic:maintenance:cleanup $MAUTIC_OPTS $CLEANUP_DAYS --dry-run >> $LOGFILE
+      php $MAUTIC_BASE/bin/console mautic:maintenance:cleanup $MAUTIC_OPTS $CLEANUP_DAYS >> $LOGFILE
    fi
 else
-   verbose "Quick updates cron script is already running - exiting"
+   verbose "Maintenance cron script is already running - exiting"
 fi
